9d6d6eb83bb13e2d3f38764bbc13782f0506e727
gabriel ruttner <gabriel.ruttner@gmail.com>
2025-06-17 18:50:07 -0700

    human in loop

---
+++ scaffolds/src/agents/human-in-the-loop/tools/send-to-slack.tool.ts
@@ -0,0 +1,22 @@
+import { pickaxe } from "@/pickaxe-client";
+import z from "zod";
+
+export const sendToSlackTool = pickaxe.tool({
+  name: "send-to-slack-tool",
+  description: "Sends a message to Slack",
+  inputSchema: z.object({
+    post: z.string(),
+    topic: z.string(),
+    targetAudience: z.string(),
+  }),
+  outputSchema: z.object({
+    messageId: z.string(),
+  }),
+  fn: async (input) => {
+    // TODO: Implement the tool
+
+    return {
+      messageId: "123",
+    };
+  },
+}); 
